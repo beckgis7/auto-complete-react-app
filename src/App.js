e1f61a0c6bf0956e0768be91e48c7fc96976e349
Evans Ofori <eofori@toldoit.com>
2021-07-28 17:36:47 +0000

    intial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { map } from 'bluebird';
 function App() {
+  const [search, setSearch] = useState([])
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const loadItems = async() => {
+      const response = await  axios.get('https://hn.algolia.com/api/v1/search_by_date?query=');
+      console.log(response.data.hits);
+      setSearch(response.data.hits)
+    }
+    loadItems();
+
+  },[])
+  const onChangeHandler =(query) => {
+    setQuery(query);
+    if (query.length>0){
+      const loadAnItem = async() => {
+        const response = await  axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=${query}&tags=story`);
+        console.log(response.data.hits);
+        setSearch(response.data.hits)
+      }
+      loadAnItem();
+
+    }
+  }
+  const searchItems = Object.keys(search).map((ent,index) =>
+    <li key={index}> {ent} </li>
+  );
+  // const searchItems = search.map((ent,index) =>
+  //     <li key={index}> {ent} </li>
+  //   );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <input className="col-md-12 input" type="text" placeholder="Search ..." 
+        onChange={e => onChangeHandler(e.target.value)}
+        value={query} 
+        />
+        <div>
+          <ul>{searchItems}</ul>
+        </div>
+      </div>
     </div>
   );
 }
