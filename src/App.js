814c8416f02f70b8936e9ab3d42a0c6221e1c693
Evans Ofori <eofori@toldoit.com>
2021-07-31 07:56:58 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@
 // import React from "react";
 import './App.css';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { 
+  // useEffect, 
+  useState 
+} from 'react';
 function App() {
   const [search, setSearch] = useState([])
   const [query, setQuery] = useState('');
-  const [final, setFinal] = useState([])
+  const [empty, setEmpty] = useState("Nothing is been searched right now")
+  console.log(empty);
 
   // const [matches, setMatches] = useState([]);
 
-  useEffect(() => {
-    const loadItems = async() => {
-      const response = await  axios.get('https://hn.algolia.com/api/v1/search?query=?query=');
-      console.log("result",response.data.hits);
-      setSearch(response.data.hits)
-    }
-    loadItems();
+  // useEffect(() => {
+  //   const loadItems = async() => {
+  //     const response = await  axios.get('https://hn.algolia.com/api/v1/search?query=?query=');
+  //     console.log("result",response.data.hits);
+  //     setSearch(response.data.hits)
+  //   }
+  //   loadItems();
 
-  },[])
+  // },[])
   const onChangeHandler =(query) => {
     setQuery(query);
     // let word_match = [];
@@ -28,16 +32,19 @@ function App() {
       //   return search.title.match(regrex); 
       // })
       // console("matches", word_match);
-      // setMatches(word_match);
+      // setMatches(word_match);]
 
+      setEmpty(null);
       const loadAnItem = async() => {
-        const response = await  axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=?query=${query}`);
+        const response = await  axios.get(`https://hn.algolia.com/api/v1/search?query=${query}&tags=(story,poll)`);
         console.log(response.data.hits);
         setSearch(response.data.hits)
       }
       loadAnItem();
-
+    } else {
+      setEmpty("Nothing is been searched right now");
     }
+    console.log(empty);
   }
   // console.log('ObjectKeys', Object.keys(search));
   // console.log('ObjectValues', Object.values(search));
@@ -51,19 +58,21 @@ function App() {
     // console.log(d.getUTCSeconds());
     // console.log(d.toLocaleString());
     return (
-    <div key={index} className="search">
-      <div className="search-header">
-        <h3 className="title">{ent.title}</h3>
-        <div className="dateTime">{time}</div>
-      </div>
-      {/* <pre className="article">{ent.comment_text}</pre> */}
-      <link className="url" href=""/>
-      <div className="search-footer">
+      <div key={index} className="search">
+        <div className="search-header">
+          <h3 className="title">{ent.title}</h3>
+          {time && <div className="dateTime">{time}</div>}
+        </div>
+        {/* <p className="article">{ent.comment_text}</p> */}
+        {/* <link className="url" href=""/> */}
+        <div className="search-footer">
         <a className="url" href={ent.url}>{ent.url}</a>
-        <div className="points">scored: {ent.points} points by <span className="author">{ent.author}</span></div>
+          <div className="points">scored: {ent.points} points by <span className="author">{ent.author}</span></div>
+        </div>
       </div>
-    </div>);
+    );
   }
+  
 
   // <li>{JSON.stringify(ent)}</li>
   );
@@ -75,9 +84,16 @@ function App() {
         onChange={e => onChangeHandler(e.target.value)}
         value={query} 
         />
-        <div>{searchItems}</div>
-        <div>
-        
+        <div className="results">
+          <h2>Results:</h2>
+          <div>{query && searchItems}</div>
+          <div>{!query && 
+            <div className="search">
+              <div className="search-header">
+                <h3 className="title">{empty}</h3>
+              </div>
+            </div>
+          }</div>
         </div>
       </div>
     </div>
